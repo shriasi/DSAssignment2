5b92d80525364bedd2b364f058c04ef1733a9af8
ashi.1015 <ayeshpramo@gmail.com>
2021-05-09 19:15:08 +0530

    initial commot

---
+++ React frontend/shoppingplatformui/src/components/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+function Home(){
+
+    return(
+
+        <div>
+
+        <div>
+            <h1>Hello world</h1>
+        </div>
+
+        </div>
+    )
+
+}
+
+export default Home;
