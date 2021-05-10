e83f7e029ae7656c29d9447f37f1adcbc6d29182
ashi.1015 <ayeshpramo@gmail.com>
2021-05-10 23:20:49 +0530

    Delivery service UI Implementation

--- React frontend/shoppingplatformui/src/App.js
+++ React frontend/shoppingplatformui/src/App.js
@@ -1,16 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import {BrowserRouter as Router, Route} from "react-router-dom";
-import Home from './components/Home'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Delivery from "./components/Delivery";
+import Test from "./components/Test";
 
 function App() {
   return (
       <Router>
+
     <div className="App">
-
-      <Route path="/" exact component = {Home}/>
-
+      <Route path="/" exact component = {Delivery}/>
+      <Route path="/test" exact component = {Test}/>
     </div>
+
       </Router>
   );
 }
---
+++ React frontend/shoppingplatformui/src/components/Delivery.js
@@ -0,0 +1,63 @@
+import React, {Component, Fragment} from 'react'
+import {Container, Row, Col, Form, Button} from "react-bootstrap";
+import "../asset/css/Style.css";
+import DeliveryImg from '../asset/image/deliver1.png'
+
+class Delivery extends Component{
+
+    render() {
+        return (
+            <div>
+                <h1>Delivery Service</h1>
+            <Fragment>
+                <Container>
+                    <Row>
+                        <Col lg={6} md={6} sm={12} className="p-5 m-auto">
+                            <div className= "deliveryBox p-5">
+                                <img className="DeliveryImg" src={DeliveryImg} alt=""/>
+                                <Form className="mt-3">
+                                    <Form.Group>
+                                        <Form.Label className="cusName">Customer Name </Form.Label>
+                                        <Form.Control type="text" placeholder="Dilshan Perera" />
+                                         <br/>
+                                    </Form.Group>
+
+                                    <Form.Group>
+                                        <Form.Label className="cusName">Customer Address </Form.Label>
+                                        <Form.Control type="address" placeholder="26 Park Street, Colombo" />
+                                        <br/>
+                                    </Form.Group>
+                                    <div className="SideBySide">
+                                        <div className="mobile">
+                                            <Form.Group>
+                                                <Form.Label className="cusName">Mobile Number </Form.Label>
+                                                <Form.Control type="mobile" placeholder="0771112223" />
+                                                <br/>
+                                            </Form.Group>
+                                        </div>
+                                        <div className="deliver">
+                                            <Form.Group>
+                                                <Form.Label className="cusName">Delivery Charges </Form.Label>
+                                                <Form.Control type="Delivery Charges" placeholder="380.00" />
+                                                <br/>
+                                            </Form.Group>
+                                        </div>
+                                    </div>
+
+                                    <Button className="btn-block" variant="success" type="submit" size="lg" block>
+                                        Deliver
+                                    </Button>
+                                </Form>
+                            </div>
+                        </Col>
+                    </Row>
+                </Container>
+
+            </Fragment>
+            </div>
+
+
+        );
+    }
+}
+export default Delivery;
